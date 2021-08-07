59d4e9a2ac8bb54632f51e57a74323d0acacee38
Crowdbotics <team@crowdbotics.com>
2021-08-07 15:05:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "snowy_shape_29611Identifier",
+  url: "https://snowy_shape_29611Identifier.botics.co",
+  api: "https://snowy_shape_29611Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
